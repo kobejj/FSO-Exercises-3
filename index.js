89394ef7135ec071285b47e7ca92a6378fb3d1dc
Kohei Nakaji <kobejj@gmail.com>
2023-07-16 21:02:19 -0400

    ready to deply

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
+const cors = require("cors");
 
+app.use(cors());
+app.use(express.static("dist"));
 app.use(express.json());
 //app.use(morgan("tiny"));
 
@@ -91,6 +94,6 @@ app.post("/api/persons", (req, res) => {
   res.json(person);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
